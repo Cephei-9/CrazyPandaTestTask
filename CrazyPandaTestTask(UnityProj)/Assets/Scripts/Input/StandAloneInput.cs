f65bf233da39de0d632bca658908e9f56dd37500
Cephei-9 <mzailogin@gmail.com>
2023-12-05 08:49:52 +0300

    Comment Input

--- CrazyPandaTestTask(UnityProj)/Assets/Scripts/Input/StandAloneInput.cs
+++ CrazyPandaTestTask(UnityProj)/Assets/Scripts/Input/StandAloneInput.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace CrazyPandaTestTask.Input
 {
+	// The input should operate not a bool but a button with different types of presses, and of course the input should
+	// be multilayered from the lower view layers to the upper layers defining the logic of the input and the implementation
+
 	public class StandAloneInput : IInput, IUpdatable
 	{
 		public GunInput RightGunInput { get; }
